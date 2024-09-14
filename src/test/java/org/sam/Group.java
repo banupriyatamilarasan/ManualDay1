85359dbc08c0e0436111595871c7325d5728832d
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 19:00:43 +0530

    created

---
+++ src/test/java/org/sam/Group.java
@@ -0,0 +1,13 @@
+package org.sam;
+
+public class Group {
+private void empName() {
+	System.out.println("vel");
+}
+private void empName1() {
+	System.out.println("mahi");
+}
+private void empName2() {
+	System.out.println("ram");
+}
+}
